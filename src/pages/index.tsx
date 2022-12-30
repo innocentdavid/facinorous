e8ace6ddf86bf706b49081898ce81442a0732df5
patriicke <patrickndayambaje4@gmail.com>
2022-12-30 10:40:00 +0200

    update(all): All page configs

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+/* eslint-disable react/no-unescaped-entities */
+import { NextPage } from "next";
+import React from "react";
+import Head from "next/head";
+const HomePage: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>OnCode</title>
+      </Head>
+      <div>BrokerChooser Clone</div>
+    </>
+  );
+};
+
+export default HomePage;

e8d81d0990f66898dcaa23e27819488adf124fbd
Innocent <paulinnocent04@gmail.com>
2023-01-03 15:15:58 +0000

    up

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,24 +1,37 @@
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
+import Image from "next/image";
 import { useState } from "react";
 import { AuthProvider } from "../context/AuthContext";
 import "./../styles/global.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-
-  return (<>
-  <div id="generalLoading" className="hidden fixed top-0 left-0 w-full h-screen place-items-center z-[999999999] text-white" style={{ background: 'rgba(0,0,0,.8)' }}>
-      <div className="text-2xl md:text-3xl lg:text-5xl flex items-center gap-3">
-        <img src="/procesos-procesos-sellcom.gif" alt="" width="20px" height="20px" className="animate-spin" />
-        <span>Loading<span className="animate-ping">...</span></span>
+  return (
+    <>
+      <div
+        id="generalLoading"
+        className="hidden fixed top-0 left-0 w-full h-screen place-items-center z-[999999999] text-white"
+        style={{ background: "rgba(0,0,0,.8)" }}
+      >
+        <div className="text-2xl md:text-3xl lg:text-5xl flex items-center gap-3">
+          <Image
+            src="/procesos-procesos-sellcom.gif"
+            alt=""
+            width={20}
+            height={20}
+          />
+          <span>
+            Loading<span className="animate-ping">...</span>
+          </span>
+        </div>
       </div>
-    </div>
-    {/* <SessionProvider session={pageProps.session}> */}
-    <AuthProvider>
+      <SessionProvider session={pageProps.session}>
+      <AuthProvider>
         <Component {...pageProps} />
-    </AuthProvider>
-    {/* </SessionProvider> */}
-  </>);
+      </AuthProvider>
+      </SessionProvider>
+    </>
+  );
 }
 
 export default MyApp;
